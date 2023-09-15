e26f4d717bd6e1d7ad7dd8df0a5148357312ecd2
Ilya Tishenko <ilya.tishencko676@yandex.ru>
2023-09-15 19:07:09 +0300

    Form submit function refactor and improvements

--- js/submit.js
+++ js/submit.js
@@ -4,10 +4,40 @@ function formSubmit(self) {
   const data = new FormData(self);
   const state = History.getState();
 
-  // необходимо изменить данную часть кода
-  let query = new URLSearchParams(data).toString();
-  let action = state.url + "?" + query;
-  // конец
+  /* ------ Необходимо изменить данную часть кода ------ */
+
+  // получаем текущий URL
+  const currentURL = new URL(state.url);
+  // получаем текущие параметры запроса
+  const urlParams = currentURL.searchParams;
+  // создаем объект для сохранения данных формы
+  const formData = {};
+
+  // обработка полей из данных формы
+  for (let [key, value] of data.entries()) {
+    // фильтруем пустые значения
+    if (value.trim()) {
+      formData[key] = value;
+    }
+    else {
+      // удаляем ключи с пустыми значениями из URL
+      urlParams.delete(key);
+    }
+  }
+
+  // добавляем оставшиеся параметры из URL в окончательные параметры
+  for (let [key, value] of urlParams.entries()) {
+    formData[key] = value;
+  }
+
+  // создаем новый URL с окончательными параметрами запроса
+  const newURL = new URL(currentURL.origin + currentURL.pathname);
+  for (let [key, value] of Object.entries(formData)) {
+    newURL.searchParams.set(key, value);
+  }
+  const action = newURL.toString();
+
+  /* ------ Конец ------ */
 
   History.pushState(null, document.title, action);
   $(".alert-dark").html(decodeURI(History.getState().url));
